f39cc7978ffe76fee7d9309a764e54af1f462184
AngeloGalav <tekken342@gmail.com>
2023-05-09 12:00:00 +0200

    Fix generator

--- instances/instance_generator.py
+++ instances/instance_generator.py
@@ -1,25 +1,37 @@
 import random
+import scipy.sparse as sp
+import numpy as np
 
 def generate_mcp_instance(n, m, max_distance, max_capacity, max_size):
-    D = [[random.randint(1, max_distance) for _ in range(n)] for _ in range(n)]
+    D = [[random.randint(1, max_distance) for _ in range(n+1)] for _ in range(n+1)]
 
     for i in range(n) :
         D[i][i] = 0
     
-    loads = [random.randint(1, max_capacity) for _ in range(m)]
-    sizes = [random.randint(1, max_size) for _ in range(n)]
+    generated = False
+
+    while not generated : 
+        loads = [random.randint(1, max_capacity) for _ in range(m)]
+        sizes = [random.randint(1, max_size) for _ in range(n)]
+        generated = np.sum(np.array(loads)) >= np.sum(np.array(sizes)) 
     
     return D, loads, sizes
 
-n = 8
-m = 6
+
+n = 30
+m = 10
 max_distance = 9
 max_capacity = 5
 max_size = 3
 D, loads, sizes = generate_mcp_instance(n, m, max_distance, max_capacity, max_size)
+
+dist_m = sp.csgraph.floyd_warshall(D, directed=True)
+print(D)
+print(dist_m)
+
 f = open("generated_unformatted_instance.txt", "w")
 f.write(str(m) + '\n')
 f.write(str(n) + '\n')
 f.write((" ".join(str(x) for x in loads)) + '\n')
 f.write((" ".join(str(x) for x in sizes)) + '\n')
-f.write(("\n".join(" ".join(str(x) for x in row) for row in D)))
+f.write(("\n".join(" ".join(str(int(x)) for x in row) for row in dist_m)))
