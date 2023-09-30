129b26452da965301ca1388d2ced1a33a510886c
AngeloGalav <agalavottib@gmail.com>
2023-09-30 20:29:07 +0200

    [General] Add very primitive draft of build_consegna.sh
    This is just a draft, do not worry abt it for now, it's just that we have to
    add it somehow

---
+++ build_consegna.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir models
+mv SMT MIP MAP SAT CP models
+
+## todo make this consegna "maker" script
+
+# moving solution
+## todo: insert dockerfile and others here
+
+tar -czvf CDMO_Proj_GalavottiAngelo_FreddiDavide_PalliNicola.tar.gz models res Dockerfile
