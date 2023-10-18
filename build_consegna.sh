a41830833ebd9f1854b77a32683c462373df90cd
AngeloGalav <agalavottib@gmail.com>
2023-10-18 16:21:31 +0200

    [General] Fix typo in build_consegna.sh

--- build_consegna.sh
+++ build_consegna.sh
@@ -6,6 +6,6 @@ cp SMT MIP MAP SAT CP models
 
 # run python scripts?
 
-tar -czvf CDMO_Proj_GalavottiAngelo_FreddiDavide_PalliNicola.tar.gz models res Dockerfile instance
+tar -czvf CDMO_Proj_GalavottiAngelo_FreddiDavide_PalliNicola.tar.gz models res Dockerfile instances
 rm -rf models
 echo "All done!"
