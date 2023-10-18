434777a40031159607ad6b2704d17a67d9fbf757
AngeloGalav <agalavottib@gmail.com>
2023-10-18 16:25:29 +0200

    [Parsers] Add parse instance to the mcp_input_parser.py lib
    Adds parse instance that allows to get variables from a plain-text based
    instance

--- mcp_input_parser.py
+++ mcp_input_parser.py
@@ -4,12 +4,14 @@ use this command:
 
 python mcp_input_parser.py [filename]
 
-it will output a parsed '.dzn' file
+it will output a MiniZinc readable '.dzn' file
 '''
-
 import sys
 
-def parse_mcp() :
+'''
+Function that outputs a MiniZinc readable '.dzn' file
+'''
+def build_dzn() :
     if len(sys.argv) > 1:
         filename = sys.argv[1]
     else:
@@ -35,7 +37,31 @@ def parse_mcp() :
 
     filename = filename.split('.')[0]
 
-    f = open(filename + ".dzn", "w") 
+    f = open(filename + ".dzn", "w")
     f.write(output)
 
-parse_mcp()
+'''
+Parses a file given as input and returns the contents as python variables.
+'''
+def parse_instance() :
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        print("no file given")
+        return 0
+
+    f = open(filename, "r")
+    m = int(f.readline())
+    n = int(f.readline())
+    l = list(map(int, f.readline().split()))
+    s = list(map(int, f.readline().split()))
+    D = []
+
+    for i in range(n+1):
+        row = list(map(int, f.readline().split()))
+        D.append(row)
+
+    # return m,n,l,s,D
+    print(m,n,l,s,D)
+
+build_dzn()
