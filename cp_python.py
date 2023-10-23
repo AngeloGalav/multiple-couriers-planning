12f3859425fa2c58a0f3884c99e3aa16a2f92053
AngeloGalav <agalavottib@gmail.com>
2023-10-23 18:13:06 +0200

    [CP] Add saveAsJson function to minizinc model loader

--- cp_python.py
+++ cp_python.py
@@ -1,14 +1,16 @@
 from minizinc import *
+from os import path
 from dzn_handlers import compute_bounds, get_values_from_dzn
+from dzn_handlers import saveAsJson
 import time
 
-def run_cp_instance(model_file, solver_name, data_file) :
-    model = Model(model_file)
-    solver = Solver.lookup(solver_name)
-    model.add_file(data_file, parse_data=True)
+def run_cp_instance(modelFile, solverName, dataFile) :
+    model = Model(modelFile)
+    solver = Solver.lookup(solverName)
+    model.add_file(dataFile, parse_data=True)
     instance = Instance(solver=solver, model=model)
 
-    with open("instances/input.dzn") as f:
+    with open("instances/instance1.dzn") as f:
         input_data = f.read()
     print(input_data)
 
@@ -23,8 +25,12 @@ def run_cp_instance(model_file, solver_name, data_file) :
 
     total_time = end-start
 
-    print(result)
+    print(result.solution.x, result.solution.objective)
     print("Time:", total_time)
+
+    # save as json with instance name
+    saveAsJson(path.basename(dataFile).split('.')[0], solverName, "res/CP/",
+               (total_time, result.solution.objective, result.solution.x))
 
 # demo
 run_cp_instance("CP/model_3.1.mzn", "gecode", "instances/instance1.dzn")
