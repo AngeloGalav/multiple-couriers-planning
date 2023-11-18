53c84642b9f8e3761969b500e99b0b757f209366
AngeloGalav <agalavottib@gmail.com>
2023-11-18 04:10:13 +0100

    [CP] Add timeout and ability to load non-dzn instances
    Finally loads .dat files properly

--- cp_python.py
+++ cp_python.py
@@ -1,26 +1,37 @@
-from minizinc import *
-from os import path
-from dzn_handlers import compute_bounds, get_values_from_dzn
-from dzn_handlers import saveAsJson
+import os
 import time
+from minizinc import *
+from datetime import timedelta
+from data_handlers import computeBounds, get_values_from_dzn, parseInstance
+from data_handlers import saveAsJson
+
+timeout = timedelta(seconds=300)
 
 def run_cp_instance(modelFile, solverName, dataFile) :
     model = Model(modelFile)
     solver = Solver.lookup(solverName)
-    model.add_file(dataFile, parse_data=True)
     instance = Instance(solver=solver, model=model)
 
-    with open("./instances/instance1.dzn") as f:
-        input_data = f.read()
-    print(input_data)
+    if (not dataFile.endswith(".dzn")) :
+        m, n, l, s, D = parseInstance(dataFile)
+    else :
+        with open(dataFile) as f:
+            input_data = f.read()
+            m, n, l, s, D = get_values_from_dzn(input_data)
 
-    m, n, _, _, D = get_values_from_dzn(input_data)
-    LB, UB = compute_bounds(D, m, n)
+    LB, UB = computeBounds(D, m, n)
 
+    # load instance data into the model
+    instance["m"] = m
+    instance["n"] = n
+    instance["l"] = l
+    instance["s"] = s
+    instance["D"] = D
     instance["LB"] = LB
     instance["UB"] = UB
+
     start = time.time()
-    result = instance.solve()
+    result = instance.solve(timeout=timeout)
     end = time.time()
 
     total_time = end-start
@@ -29,19 +40,20 @@ def run_cp_instance(modelFile, solverName, dataFile) :
     print("Time:", total_time)
 
     # save as json with instance name
-    saveAsJson(path.basename(dataFile).split('.')[0], solverName, "res/CP/",
+    saveAsJson(os.path.basename(dataFile).split('.')[0], solverName, "res/CP/",
                (total_time, result.solution.objective, result.solution.x))
 
 def modify_model_heuristics(modelFile, onCopy=True):
     strategies = ["input_order", "first_fail", "smallest", "dom_w_deg"]
     heuristics = ["indomain_min", "indomain_median", "indomain_random", "indomain_split"]
+    restart = ["restart_linear(<scale>)", "restart_geometric(<base>,<scale>)", "restart_luby(<scale>)"]
 
     # solve :: seq_search([
     #          int_search(s, smallest, indomain_min),
     #          int_search([end], input_order, indomain_min)])
     #   minimize end
 
-    # works on the co   py of a file, not directly on the model
+    # works on the copy of a file, not directly on the model
     if onCopy:
         ...
 
@@ -49,4 +61,4 @@ def modify_model_heuristics(modelFile, onCopy=True):
 
 
 # demo
-run_cp_instance("CP/model_3.1.mzn", "gecode", "./instances/instance1.dzn")
+run_cp_instance("CP/model_3.1.mzn", "gecode", "./instances/inst02.dat")
