bf21051c4c21a6405068eceaf1f4397aa88f7d4c
AngeloGalav <agalavottib@gmail.com>
2023-12-06 17:10:31 +0100

    [General] Add final consegna building script

--- build_consegna.sh
+++ build_consegna.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 echo "Initiating consegna building..."
-mkdir models
-cp -r SMT MIP MAP SAT CP models
 
-# run python scripts?
+if [ -f .submissionignore ]; then
+    # Create a temporary tarignore file
+    tarignore=".tarignore"
+    touch $tarignore
 
-tar -czvf CDMO_Proj_GalavottiAngelo_FreddiDavide_PalliNicola.tar.gz models res Dockerfile instances docker_bins
-rm -rf models
-echo "All done!"
+    # read each line from file
+    while IFS= read -r line || [[ -n "$line" ]]; do
+        echo "$line" >> $tarignore
+    done < .submissionignore
+
+    tar czf CDMO_Proj_FreddiDavide_GalavottiAngelo_PalliNicola.tar.gz -X $tarignore *
+    
+    rm $tarignore
+else
+    tar czf CDMO_Proj_FreddiDavide_GalavottiAngelo_PalliNicola.tar.gz *
+fi
+
+echo "Created archive: CDMO_Proj_FreddiDavide_GalavottiAngelo_PalliNicola.tar.gz"
